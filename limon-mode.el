b6e11e2079a716eecf1184b2c25037ab70e7d700
Can Aknesil <can.aknesil@ericsson.com>
2019-07-23 18:21:03 +0200

    Complete syntax table, some of syntax highlights, template for indentation.

--- limon-mode.el
+++ limon-mode.el
@@ -1,5 +1,108 @@
 ;;
-;; CONSTANT PARAMETERS
+;; UTILITY
+;;
+(defun set-char-table-range-list (range-list table)
+  (mapcar (lambda (range)
+	    (set-char-table-range table
+				  (car range)
+				  (string-to-syntax (cdr range))))
+	  range-list))
+
+
+;;
+;; SYNTAX TABLE
+;;
+
+(defconst limon-mode-syntax-table-range-list
+  '(;; White character
+    (?\s . "-") ;;space
+    (?\t . "-") ;;tab
+    ;;(?\n . "-") ;; Used as comment, look below.
+
+    ;; Word constituents
+    ((?a . ?z) . "w")
+    ((?A . ?Z) . "w")
+    ((?0 . ?9) . "w")
+
+    ;; Symbol constituents
+    (?_ . "_")
+    (?. . "_")
+
+    ;; Ponctuation
+    (?= . ".")
+    (?< . ".")
+    (?> . ".")
+    (?! . ".")
+    (?+ . ".")
+    (?- . ".")
+    (?* . ". 23") ;; In addition to being in class _, the second
+		  ;; character of a two-character comment-start
+		  ;; sequence (2), the first character of such a
+		  ;; comment-end sequence (3).
+    
+    (?/ . ". 124b") ;; In addition to being in class ., the start of a
+                    ;; two-character comment-start sequence (1), the
+		    ;; second character of such a sequence (2), the
+		    ;; second character of a two-character comment-end
+		    ;; sequence (4) where the comment sequence if of
+		    ;; style "b" (b).
+    (?% . ".")
+    (?& . ".")
+    (?| . ".")
+    (?@ . ".")
+    (?? . ".")
+    (?: . ".")
+    (?# . ".")
+
+    ;; Open delimiter character
+    (?\( . "(")
+    (?\[ . "(")
+    (?\{ . "(")
+
+    ;; Close delimiter character
+    (?\) . ")")
+    (?\] . ")")
+    (?\} . ")")
+
+    ;; String quote character
+    (?\" . "\"")
+    (?' . "\"")
+    
+    ;; Escape character
+    (?\\ . "\\")
+
+    ;; Character quote character
+    
+    ;; Paired delimiter
+
+    ;; Expression prefix
+
+    ;; Comment starter
+    ;; (?* . "_ 23") ;; Above
+    ;; (?/ . "_ 124b") 
+    
+    ;; Comment ender
+    (?\n . "> b") ;; Comment-end character of comment style "b".
+    
+    ;; Generic comment delimiter
+
+    ;; Generic string delimiter
+
+    ;; Inherit from `standard-syntax-table'
+
+    ))
+
+    
+(defvar limon-mode-syntax-table
+  (make-char-table 'syntax-table nil)) ;; Empty syntax table
+
+(set-char-table-range-list limon-mode-syntax-table-range-list
+			   limon-mode-syntax-table)
+			   
+
+
+;;
+;; SYNTAX HIGHLIGHTING
 ;;
 
 (defconst limon-keyword
@@ -21,16 +124,18 @@
    'symbols))
 
 (defconst limon-warning
-  "error")
+  (regexp-opt
+   '("error")
+   'symbols))
 
 (defconst limon-keychar
   (regexp-opt
    '("?"
      "#")
-   t))
+   t)) ;;group
 
 (defconst limon-function-name
-  (regexp-opt '("@") t))
+  (regexp-opt '("@") t)) ;;group
 
 
 (defconst limon-operator
@@ -56,13 +161,17 @@
      ">"
      "&"
      "|")
-   t))
+   t)) ;;group
 
 (defconst limon-negation-char
-  "!")
+  (regexp-opt
+   '("!")
+   t)) ;;group
 
 (defconst limon-bracket
-  (regexp-opt '("[" "]")))
+  (regexp-opt
+   '("[" "]")
+   t)) ;;group
 
 (defconst limon-constant-word
   (regexp-opt
@@ -71,8 +180,14 @@
      "null")
    'symbols))
 
+;; AFTER THIS DOES NOT WORK ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defconst limon-constant-int
-  "\\<\\([0-9]+\\|0b[01]+\\|0x[0-9A-Fa-f]+\\)\\>")
+  (regexp-opt
+   '("[0-9]+"
+     "0b[01]+"
+     "0x[0-9A-Fa-f]+")
+   'symbols))
 
 (defconst limon-constant-float
   "\\<[0-9]*\\.[0-9]+\\([pP][1-9][0-9]*\\)?\\>")
@@ -87,15 +202,94 @@
   "\\<\\(:[A-Za-z_][A-Za-z0-9_]*\\)\\>")
 
 (defconst limon-variable-name
-  "\\<\\([A-Za-z_][A-Za-z0-9_]*\\)\\>")
+  (regexp-opt
+   '("\\(?:def\\s-\\)\\s_")
+   'symbols))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defconst limon-colon
   "\\(:\\)")
 
 
+(defconst limon-font-lock-keywords
+  (list
+   (cons limon-keyword 'font-lock-keyword-face)
+   (cons limon-warning 'font-lock-warning-face)
+   (cons limon-keychar 'font-lock-keyword-face)
+   (cons limon-function-name 'font-lock-function-name-face)
+   (cons limon-bracket 'font-lock-function-name-face)
+   (cons limon-constant-word 'font-lock-constant-face)
+   ;;(cons limon-constant-float 'font-lock-constant-face)
+   ;;(cons limon-constant-float-bin 'font-lock-constant-face)
+   ;;(cons limon-constant-float-hex 'font-lock-constant-face)
+   (cons limon-constant-int 'font-lock-constant-face)
+   ;;(cons limon-symbol 'font-lock-type-face)
+   ;;(cons limon-variable-name 'font-lock-variable-name-face)
+   ;;(cons limon-colon 'font-lock-keyword-face)
+   (cons limon-negation-char 'font-lock-negation-char-face))
+  "Highlighting expressions for Limon mode")
+
+
 ;;
-;; UTILITY
+;; INDENTATION
 ;;
+
+(defun limon-indent-line ()
+  (interactive)
+  (save-excursion
+    (beginning-of-line) ;; Set point to beginning of line.
+
+    (cond
+
+     ;; Beginning of buffer, indent to 0.
+     ((bobp) 
+      (indent-line-to 0))
+
+     
+     
+     ;; Default, indent as previous line.
+     (t
+      (let ((prev-line-indent
+	     (progn
+	       (forward-line -1) ;; move up
+	       (current-indentation))))
+	(forward-line 1) ;; move down
+	(indent-line-to prev-line-indent))))))	 
+	 
+      
+      
+
+(defun limon-indent-line-temp ()
+  "Indent current line as WPDL code."
+  (interactive)
+  (beginning-of-line)
+  (if (bobp)
+      (indent-line-to 0)		   ; First line is always non-indented
+    (let ((not-indented t) cur-indent)
+      (if (looking-at "^[ \t]*END_") ; If the line we are looking at is the end of a block, then decrease the indentation
+	  (progn
+	    (save-excursion
+	      (forward-line -1) ;; Move up
+	      (setq cur-indent (- (current-indentation) default-tab-width)))
+	    (if (< cur-indent 0) ; We can't indent past the left margin
+		(setq cur-indent 0)))
+	(save-excursion
+	  (while not-indented ; Iterate backwards until we find an indentation hint
+	    (forward-line -1)
+	    (if (looking-at "^[ \t]*END_") ; This hint indicates that we need to indent at the level of the END_ token
+		(progn
+		  (setq cur-indent (current-indentation))
+		  (setq not-indented nil))
+	      (if (looking-at "^[ \t]*\\(PARTICIPANT\\|MODEL\\|APPLICATION\\|WORKFLOW\\|ACTIVITY\\|DATA\\|TOOL_LIST\\|TRANSITION\\)") ; This hint indicates that we need to indent an extra level
+		  (progn
+		    (setq cur-indent (+ (current-indentation) default-tab-width)) ; Do the actual indenting
+		    (setq not-indented nil))
+		(if (bobp)
+		    (setq not-indented nil)))))))
+      (if cur-indent
+	  (indent-line-to cur-indent)
+	(indent-line-to 0))))) ; If we didn't see an indentation hint, then allow no indentation
 
 
 ;;
@@ -117,40 +311,6 @@
 
 
 ;;
-;; SYNTAX HIGHLIGHTING
-;;
-
-(defconst limon-font-lock-keywords
-  (list
-   (cons limon-keyword 'font-lock-keyword-face)
-   (cons limon-warning 'font-lock-warning-face)
-   (cons limon-keychar 'font-lock-keyword-face)
-   (cons limon-function-name 'font-lock-function-name-face)
-   (cons limon-bracket 'font-lock-function-name-face)
-   (cons limon-constant-word 'font-lock-constant-face)
-   (cons limon-constant-float 'font-lock-constant-face)
-   (cons limon-constant-float-bin 'font-lock-constant-face)
-   (cons limon-constant-float-hex 'font-lock-constant-face)
-   (cons limon-constant-int 'font-lock-constant-face)
-   (cons limon-symbol 'font-lock-type-face)
-   (cons limon-variable-name 'font-lock-variable-name-face)
-   (cons limon-colon 'font-lock-keyword-face)
-   (cons limon-negation-char 'font-lock-negation-char-face))
-  "Highlighting expressions for Limon mode")
-
-
-(defvar limon-mode-syntax-table
-  (let ((limon-mode-syntax-table
-	 (make-syntax-table))); c-mode-syntax-table)))
-    (modify-syntax-entry ?_ "w" limon-mode-syntax-table)
-    (modify-syntax-entry ?/ ". 124b" limon-mode-syntax-table)
-    (modify-syntax-entry ?* ". 23" limon-mode-syntax-table)
-    (modify-syntax-entry ?\n "> b" limon-mode-syntax-table)
-    limon-mode-syntax-table)
-  "Syntax table for limon-mode")
-
-
-;;
 ;; THE ENTRY FUNCTION
 ;;
 
@@ -161,7 +321,7 @@
   (set-syntax-table limon-mode-syntax-table)
   (use-local-map limon-mode-map)
   (set (make-local-variable 'font-lock-defaults) '(limon-font-lock-keywords))
-  ;(set (make-local-variable 'indent-line-function) 'limon-indent-line)
+  (set (make-local-variable 'indent-line-function) 'limon-indent-line)
   (setq major-mode 'limon-mode)
   (setq mode-name "Limon")
   (run-hooks 'limon-mode-hook))
